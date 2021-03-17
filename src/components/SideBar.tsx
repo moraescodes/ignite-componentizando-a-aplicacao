56c93cd0d014733b13418a260aab41e081043fc9
Carlos Moraes <carlos@pop-os.localdomain>
2021-03-17 19:38:15 -0300

    :tada: Desafio Completado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
+import { Button } from './Button';
+interface GenreResponse {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps{
+  genres: Array<GenreResponse>;
+  selectedGenreId: number;
+  onClickButton: (id:number) => void;
+}
+export function SideBar({ genres, selectedGenreId, onClickButton }:SideBarProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              id={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => onClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
